e9b2b75347675f51581e7504857c2949bb9b971d
ASolaDev <antoniosola991@gmail.com>
2025-04-30 09:42:55 +0200

    Añadida página 404 con estilos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { MenuComponent } from './layout/menu.component';
 import { FacturasComponent } from './pages/facturas/facturas.component';
 import { CrearFacturaComponent } from './pages/facturas/crear-factura/crear-factura.component';
+import { Page404Component } from './pages/page404/page404.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -16,13 +17,14 @@ export const routes: Routes = [
       { path: 'inicio', component: DashboardComponent },
       { path: 'facturas', component: FacturasComponent },
       { path: 'facturas/crear', component: CrearFacturaComponent },
-    //   { path: 'gastos', component: GastosComponent },
-    //   { path: 'estadisticas', component: EstadisticasComponent },
-    //   { path: 'prevision', component: PrevisionComponent },
-    //   { path: 'tesoreria', component: TesoreriaComponent },
-    //   { path: 'notificaciones', component: NotificacionesComponent },
-    //   { path: 'agenda', component: AgendaComponent },
-    //   { path: 'exportar', component: ExportacionComponent },
+      { path: '**', component: Page404Component },
+      //   { path: 'gastos', component: GastosComponent },
+      //   { path: 'estadisticas', component: EstadisticasComponent },
+      //   { path: 'prevision', component: PrevisionComponent },
+      //   { path: 'tesoreria', component: TesoreriaComponent },
+      //   { path: 'notificaciones', component: NotificacionesComponent },
+      //   { path: 'agenda', component: AgendaComponent },
+      //   { path: 'exportar', component: ExportacionComponent },
     ]
   }
 ];
