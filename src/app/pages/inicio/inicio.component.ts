27355c55823800da45719ae51275ef361103deda
ASolaDev <antoniosola991@gmail.com>
2025-06-03 21:02:27 +0200

    Modificado componente inicio

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-inicio',
-    imports: [],
-    templateUrl: './inicio.component.html'
+  selector: 'app-inicio',
+  imports: [],
+  templateUrl: './inicio.component.html',
+  styleUrl: './inicio.component.css'
 })
-
 export class InicioComponent {
 
 }
