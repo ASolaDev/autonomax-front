913d78db4f311cde41ea21cb962d5cc01aa2a6d4
Zaira <zairaamariaa@gmail.com>
2025-06-15 19:39:56 +0200

    creada seccion de ajustes

--- src/app/pages/ajustes/ajustes.component.ts
+++ src/app/pages/ajustes/ajustes.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CambiarPasswordComponent } from './cambiar-password/cambiar-password.component';
 
 @Component({
-    selector: 'app-ajustes',
-    imports: [],
-    templateUrl: './ajustes.component.html'
+  selector: 'app-ajustes',
+  imports: [CambiarPasswordComponent],
+  templateUrl: './ajustes.component.html',
+  styleUrls: []
 })
-
 export class AjustesComponent {
+  @ViewChild('modal') modal!: CambiarPasswordComponent;
 
+  abrirModal() {
+    this.modal.abrir();
+  }
+
+  cerrarModal() {
+    console.log('Modal cerrado');
+  }
 }
