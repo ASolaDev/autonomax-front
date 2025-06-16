8a7286250209c38355bf1196186f9e1dfbdc7772
PericoSG <4803567@alu.murciaeduca.es>
2025-06-16 19:49:20 +0200

    Se puede ver todos los proveedores que hay en back

--- src/app/pages/proveedores/proveedores.component.ts
+++ src/app/pages/proveedores/proveedores.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Proveedores } from '../../models/Proveedores';
+import { ProveedoresService } from '../../services/proveedores.service';
 
 @Component({
   selector: 'app-proveedores',
@@ -9,10 +11,26 @@ import { Router } from '@angular/router';
   styleUrl: './proveedores.component.css'
 })
 export class ProveedoresComponent {
-  constructor(private router: Router){}
+    private proveedores:Proveedores[] = [];
+  constructor(private router: Router, private proveedoresService:ProveedoresService){}
 
+  ngOnInit(){
+    this.getProveedoresAPI();
+  }
   onCrearProveedor() {
         console.log('Redirigiendo a /crear');
         this.router.navigate(['proveedores/crear']);
     }
+
+  getProveedoresAPI(){
+    return this.proveedoresService.getProveedores().subscribe(
+        (proveedores: Proveedores[]) => {
+      this.proveedores = proveedores;
+    }
+    )
+  }
+
+  getProveedores(){
+    return this.proveedores;
+  }
 }
