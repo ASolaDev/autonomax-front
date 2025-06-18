a90347979809c4f285c55f0d749c85052e17d7b2
Zaira <zairaamariaa@gmail.com>
2025-06-18 23:06:03 +0200

    menu hamburguesa funcionando + color calendario cambiado

--- src/app/layout/menu.component.ts
+++ src/app/layout/menu.component.ts
@@ -1,27 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-menu',
-    imports: [RouterModule],
-    templateUrl: './menu.component.html'
+  selector: 'app-menu',
+  imports: [RouterModule, CommonModule],
+  templateUrl: './menu.component.html',
 })
 
 export class MenuComponent {
+  constructor(private auth: AuthService, private router: Router) {}
 
-    constructor(private auth: AuthService, private router: Router) { }
+  isMenuOpen: boolean = false;
 
-    logout() {
-        this.auth.logout().subscribe(
-            () => {
-                this.router.navigate(['/login'])
-            },
-            error => {
-                sessionStorage.clear()
-                this.router.navigate(['/login'])
-            }
-        )
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  logout() {
+    this.auth.logout().subscribe(
+      () => {
+        this.router.navigate(['/login']);
+      },
+      error => {
+        sessionStorage.clear();
+        this.router.navigate(['/login']);
+      }
+    );
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    const width = event.target.innerWidth;
+    if (width >= 768 && this.isMenuOpen) {
+      this.isMenuOpen = false;
     }
-}
+  }
 
+  ngOnInit() {
+    if (window.innerWidth >= 768) {
+      this.isMenuOpen = false;
+    }
+  }
+
+  onNavigate() {
+    if (window.innerWidth < 768) {
+        this.isMenuOpen = false;
+    }
+  }
+}
