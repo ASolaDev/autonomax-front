44706c7eb04acfad0c769c42c66dd2bf7cd55557
Zaira <zairaamariaa@gmail.com>
2025-06-18 13:11:30 +0200

    menu hamgurguesa funcional y responsive

--- src/app/layout/menu.component.ts
+++ src/app/layout/menu.component.ts
@@ -1,27 +1,36 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-menu',
-    imports: [RouterModule],
+    imports: [RouterModule, CommonModule],
     templateUrl: './menu.component.html'
 })
 
 export class MenuComponent {
+  isMobileMenuOpen = false;
 
-    constructor(private auth: AuthService, private router: Router) { }
+  constructor(private auth: AuthService, private router: Router) { }
 
-    logout() {
-        this.auth.logout().subscribe(
-            () => {
-                this.router.navigate(['/login'])
-            },
-            error => {
-                sessionStorage.clear()
-                this.router.navigate(['/login'])
-            }
-        )
-    }
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
+  }
+
+  logout() {
+    this.auth.logout().subscribe(
+      () => this.router.navigate(['/login']),
+      () => {
+        sessionStorage.clear();
+        this.router.navigate(['/login']);
+      }
+    );
+  }
 }
+
 
