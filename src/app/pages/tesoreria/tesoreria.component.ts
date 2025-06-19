449688436a9246f15a2499e7544943ab1989cc27
ASolaDev <antoniosola991@gmail.com>
2025-06-19 22:24:15 +0200

    Modificacion totales tesorería

--- src/app/pages/tesoreria/tesoreria.component.ts
+++ src/app/pages/tesoreria/tesoreria.component.ts
@@ -11,12 +11,30 @@ import { CommonModule } from '@angular/common';
 })
 export class TesoreriaComponent implements OnInit {
     facturas: Factura[] = [];
+    saldoTotal: number = 0;
+    ingresosMesActual: number = 0;
 
     constructor(private facturaService: FacturaService) { }
 
     ngOnInit() {
         this.facturaService.getFacturasAPI().subscribe(facturas => {
             this.facturas = facturas;
+            this.calcularTotales();
         });
+    }
+
+    calcularTotales() {
+        const hoy = new Date();
+        const mesActual = hoy.getMonth();
+        const anioActual = hoy.getFullYear();
+
+        this.saldoTotal = this.facturas.reduce((acc, f) => acc + (f.total || 0), 0);
+
+        this.ingresosMesActual = this.facturas
+            .filter(f => {
+                const fecha = new Date(f.fechaEmision);
+                return fecha.getMonth() === mesActual && fecha.getFullYear() === anioActual;
+            })
+            .reduce((acc, f) => acc + (f.total || 0), 0);
     }
 }
